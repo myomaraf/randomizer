a3735fdbebe11e30392c062ec6e12daed6264667
Clinton Agburum <clinsgfx@gmail.com>
2026-02-23 13:03:55 +0100

    fixed run migration error on production

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,37 @@
 <?php
 
 use App\Http\Controllers\RafflesController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
+});
+
+
+Route::middleware(['throttle:3,1'])->get('/run-migration', function (Request $request) {
+    $expectedToken = (string) env('MAINTENANCE_ROUTE_TOKEN', '');
+    $providedToken = (string) $request->query('token', '');
+
+    if ($expectedToken === '' || ! hash_equals($expectedToken, $providedToken)) {
+        abort(404);
+    }
+
+    $output = [];
+
+    try {
+        Artisan::call('migrate', ['--force' => true]);
+        $output[] = '[migrate] OK';
+        $artisanOutput = trim(Artisan::output());
+        if ($artisanOutput !== '') {
+            $output[] = $artisanOutput;
+        }
+    } catch (\Throwable $e) {
+        $output[] = '[migrate] ERROR: ' . $e->getMessage();
+    }
+
+    return response('<pre>' . e(implode("\n\n", $output)) . '</pre>');
 });
 
 Route::get('/raffles', [RafflesController::class, 'index'])->name('raffles.index');
